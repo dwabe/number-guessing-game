2812c9126466063cfcdb4f1edc1429ff9eda3b3e
Bartek Błęcki <bartek@blecki.pl>
2019-05-08 19:12:16 +0200

    Add focus() method

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ var guessField = document.querySelector('.guessField');
 var guessCount = 1;
 var resetButton;
 
+guessField.focus();
+
 function checkGuess() {
   var userGuess = Number(guessField.value);
   if (guessCount === 1) {
